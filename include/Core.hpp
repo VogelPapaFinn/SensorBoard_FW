82e00b946f35f81d52a781082af18b7f18dba1be
Finn Ganser <38922171+VogelPapaFinn@users.noreply.github.com>
2026-06-09 20:45:57 +0200

    Changed Core.hpp endline symbol (to CRLF)

--- include/Core.hpp
+++ include/Core.hpp
@@ -1,1 +1,76 @@
-#pragma once// Project includes#include "Can.hpp"#include "Config.hpp"#include "Driver/Display.hpp"// espidf includes#include "esp_adc/adc_oneshot.h"/* *	Public constexpr */constexpr gpio_num_t GPIO_DISPLAY1 = GPIO_NUM_13;constexpr gpio_num_t GPIO_DISPLAY2 = GPIO_NUM_14;constexpr gpio_num_t GPIO_DISPLAY3 = GPIO_NUM_21;/* *	Class */class Core{public:	static Core* get();	void setMainEventQueue(QueueHandle_t queue);	QueueHandle_t getMainEventQueue() const;	std::vector<Display>* getDisplays();	adc_oneshot_unit_handle_t* getAdc();	ArduinoJson::JsonDocument* getConfig() const;	void setWifi	void saveConfig() const;	/*	 *	CAN related functions	 */	Can* getCan() const;private:	/*	 *	Instances	 */	static Core* self_;	Can* can_ = nullptr;	/*	 *	Private Functions	 */	Core();	/*	 *	Private Variables	 */	QueueHandle_t mainEventQueue_ = nullptr;	std::vector<Display> displays_ = {		Display(GPIO_DISPLAY1, CAN_MASTER_ID + 1, 1, true),		Display(GPIO_DISPLAY2, CAN_MASTER_ID + 2, 0, false),		Display(GPIO_DISPLAY3, CAN_MASTER_ID + 3, 2, true),	};	adc_oneshot_unit_handle_t adc1Handle_;	Filesystem* filesystem_ = nullptr;	Config* config_ = nullptr;	ArduinoJson::JsonDocument* jsonConfig_ = nullptr;};
+#pragma once
+
+// Project includes
+#include "Can.hpp"
+#include "Config.hpp"
+#include "Driver/Display.hpp"
+
+// espidf includes
+#include "esp_adc/adc_oneshot.h"
+
+/*
+ *	Public constexpr
+ */
+constexpr gpio_num_t GPIO_DISPLAY1 = GPIO_NUM_13;
+constexpr gpio_num_t GPIO_DISPLAY2 = GPIO_NUM_14;
+constexpr gpio_num_t GPIO_DISPLAY3 = GPIO_NUM_21;
+
+/*
+ *	Class
+ */
+class Core
+{
+public:
+	static Core* get();
+
+	void setMainEventQueue(QueueHandle_t queue);
+
+	QueueHandle_t getMainEventQueue() const;
+
+	std::vector<Display>* getDisplays();
+
+	adc_oneshot_unit_handle_t* getAdc();
+
+	ArduinoJson::JsonDocument* getConfig() const;
+
+	void setWifi
+
+	void saveConfig() const;
+
+	/*
+	 *	CAN related functions
+	 */
+	Can* getCan() const;
+
+private:
+	/*
+	 *	Instances
+	 */
+	static Core* self_;
+
+	Can* can_ = nullptr;
+
+	/*
+	 *	Private Functions
+	 */
+	Core();
+
+	/*
+	 *	Private Variables
+	 */
+	QueueHandle_t mainEventQueue_ = nullptr;
+
+	std::vector<Display> displays_ = {
+		Display(GPIO_DISPLAY1, CAN_MASTER_ID + 1, 1, true),
+		Display(GPIO_DISPLAY2, CAN_MASTER_ID + 2, 0, false),
+		Display(GPIO_DISPLAY3, CAN_MASTER_ID + 3, 2, true),
+	};
+
+	adc_oneshot_unit_handle_t adc1Handle_;
+
+	Filesystem* filesystem_ = nullptr;
+
+	Config* config_ = nullptr;
+
+	ArduinoJson::JsonDocument* jsonConfig_ = nullptr;
+};
