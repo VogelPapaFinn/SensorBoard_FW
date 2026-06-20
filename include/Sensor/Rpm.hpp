00eb9242fb4e2b2ddc34629efd0a77743aae2832
Finn Ganser <finn.ganser@gmx.de>
2026-06-20 15:50:57 +0200

    Simplified and improved the accuracy of the RPM measurement a lot

--- include/Sensor/Rpm.hpp
+++ include/Sensor/Rpm.hpp
@@ -3,6 +3,9 @@
 // Project includes
 #include "ActiveSensor.hpp"
 
+// C++ includes
+#include <list>
+
 // espidf includes
 #include "freertos/FreeRTOS.h"
 
@@ -20,18 +23,13 @@ public:
 
 private:
 	/*
-	 *	Private Functions
-	 */
-	void calculateRpm();
-
-	/*
 	 *	Private Variables
 	 */
 	int64_t lastFallingEdgeTime_ = 0;
 	int64_t fallingEdgeTime_ = 0;
 	portMUX_TYPE mux_ = portMUX_INITIALIZER_UNLOCKED;
 
-	double hz_ = 0.0;
+	float hz_ = 0.0;
 
-	uint16_t rpm_ = 0;
+	std::list<uint16_t> lastRpm_ = {0, 0, 0};
 };
--- src/Sensor/Rpm.cpp
+++ src/Sensor/Rpm.cpp
@@ -25,12 +25,6 @@ constexpr float MPH_TO_KMH = 1.60934;
 
 constexpr uint16_t MAX_RPM = 8000;
 
-constexpr FrequencyMultiplierTuple_t FREQ_MULTIPLIER_TUPLES[] = {
-	{8, 50},	  {11, 45.45},	{17, 41.18},  {25, 40.0},	{56, 34.48},  {92, 32.61},
-	{123, 32.52}, {157, 31.85}, {188, 31.91}, {220, 31.82}, {262, 30.54},
-};
-constexpr uint8_t AMOUNT_FREQ_MULTIPLIER_TUPLES = sizeof(FREQ_MULTIPLIER_TUPLES) / sizeof(FREQ_MULTIPLIER_TUPLES[0]);
-
 /*
  *	Public Function Implementations
  */
@@ -56,60 +50,37 @@ int Rpm::get()
 
 	// Calculate RPM
 	const float seconds = static_cast<float>(time) / 1000000.0f;
-	hz_ = round(1.0 / seconds);
-	calculateRpm();
+	hz_ = 1.0f / seconds;
+	if (hz_ <= 0.0f) {
+		return 0;
+	}
 
-	return rpm_;
+	// Calculate the rpm
+	uint16_t rpm = static_cast<uint16_t>((hz_ * 60.0f) / 2.0f);
+	if (rpm > MAX_RPM) {
+		rpm = 0;
+	}
+
+	/*
+	 *	Median filter
+	 */
+	lastRpm_.pop_front();
+	lastRpm_.push_back(rpm);
+	lastRpm_.sort();
+
+	return *(++lastRpm_.begin());
 }
 
 void Rpm::cb()
 {
-	portENTER_CRITICAL_ISR(&mux_);
-	lastFallingEdgeTime_ = fallingEdgeTime_;
-	fallingEdgeTime_ = esp_timer_get_time();
-	portEXIT_CRITICAL_ISR(&mux_);
-}
-
-/*
- *	Private Functions Implementations
- */
-void Rpm::calculateRpm()
-{
-	if (hz_ <= 0) {
-		return;
-	}
-
-	double multiplier = 30.0;
-
-	// Frequency is lower than the first data point
-	if (hz_ <= FREQ_MULTIPLIER_TUPLES[0].freq) {
-		multiplier = FREQ_MULTIPLIER_TUPLES[0].multiplier;
-	}
-
-	// Its Frequency is higher than the last data point
-	else if (hz_ >= FREQ_MULTIPLIER_TUPLES[AMOUNT_FREQ_MULTIPLIER_TUPLES - 1].freq) {
-		multiplier = FREQ_MULTIPLIER_TUPLES[AMOUNT_FREQ_MULTIPLIER_TUPLES - 1].multiplier;
-	}
-
-	// Interpolate the multiplier
-	else {
-		for (uint8_t i = 0; i < AMOUNT_FREQ_MULTIPLIER_TUPLES; i++) {
-			if (hz_ >= FREQ_MULTIPLIER_TUPLES[i].freq && hz_ <= FREQ_MULTIPLIER_TUPLES[i + 1].freq) {
-				const double x0 = FREQ_MULTIPLIER_TUPLES[i].freq;
-				const double y0 = FREQ_MULTIPLIER_TUPLES[i].multiplier;
-				const double x1 = FREQ_MULTIPLIER_TUPLES[i + 1].freq;
-				const double y1 = FREQ_MULTIPLIER_TUPLES[i + 1].multiplier;
-
-				// Linear interpolation see https://en.wikipedia.org/wiki/Linear_interpolation
-				multiplier = y0 + (hz_ - x0) * ((y1 - y0) / (x1 - x0));
-				break;
-			}
-		}
-	}
-
-	// Calculate the rpm and return it
-	rpm_ = static_cast<uint16_t>(hz_ * multiplier);
-	if (rpm_ > MAX_RPM) {
-		rpm_ = 0;
+    /*
+     *	Debouncing
+     */
+	const auto now = esp_timer_get_time();
+	if ((now - fallingEdgeTime_) > 2000) { // With a max of 8000rpm the min time between each trigger is above 2000us
+		portENTER_CRITICAL_ISR(&mux_);
+		lastFallingEdgeTime_ = fallingEdgeTime_;
+		fallingEdgeTime_ = now;
+		portEXIT_CRITICAL_ISR(&mux_);
 	}
 }
